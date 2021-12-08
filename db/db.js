16b4f5c6fda97982d0d04cde6a2d59abe1c7ffd2
MaryamAlanzi <maryam.sayyar.rae.js@tuwaiq.edu.sa>
2021-12-08 14:32:38 +0300

    new

---
+++ db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGOURL).then(
+  () => {
+    console.log("DB connected");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
